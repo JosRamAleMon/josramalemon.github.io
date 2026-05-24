0891786f916160e3d431ae0c01fda6359e0d64b9
JosRamAleMon <ramirezmonsalvejoseph@gmail.com>
2026-05-23 19:53:48 -0500

    Update javaPortafolio.js

--- javaPortafolio.js
+++ javaPortafolio.js
@@ -1,55 +1,35 @@
 // FUNCION PARA CAMBIAR SECCIONES
-
-
 function mostrarSeccion(idSeccion, botonClickeado) {
 
     // Ocultar secciones
-
     const secciones = document.querySelectorAll('.contenido-seccion');
 
     secciones.forEach(seccion => {
         seccion.classList.add('d-none');
     });
 
-
     // Mostrar seccion seleccionada (menu header)
-
-    document.getElementById(idSeccion)
-            .classList.remove('d-none');
-
+    document.getElementById(idSeccion).classList.remove('d-none');
 
     // Descartar boton activo si se deja de seleccionar
-
     const botones = document.querySelectorAll('.menu-btn');
 
     botones.forEach(boton => {
-
         boton.classList.remove('btn-light');
         boton.classList.remove('text-dark');
-
         boton.classList.add('btn-outline-light');
-
     });
 
-
     // Activar boton al dar click
-
     botonClickeado.classList.remove('btn-outline-light');
-
     botonClickeado.classList.add('btn-light');
     botonClickeado.classList.add('text-dark');
-
 }
 
-
 // ==========================================
-// MOSTRAR SEMANA 1 AL INICIAR
+// MOSTRAR FASE 1 AL INICIAR
 // ==========================================
-
 document.addEventListener('DOMContentLoaded', () => {
-
     const primerBoton = document.querySelector('.menu-btn');
-
-    mostrarSeccion('semana1', primerBoton);
-
+    mostrarSeccion('fase1', primerBoton);
 });
